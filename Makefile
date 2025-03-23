50a9815bb471f8ed4f500d66d2c926a1a6807156
Ciaran <cslemon96@gmail.com>
2025-03-23 18:29:33 +0200

    feat(main.c): basic spinning cube

--- Makefile
+++ Makefile
@@ -1,12 +1,23 @@
 build: 
 	gcc main.c \
 		-std=c2x \
+		-Wall \
+		-Wextra \
+		-Werror \
+		-lraylib \
+		-g \
+		-o shaderedit
+
+build-strict:
+	gcc main.c \
+		-std=c2x \
 		-fsanitize=address \
 		-Wall \
 		-Wextra \
 		-Werror \
 		-pedantic \
 		-lraylib \
+		-g \
 		-o shaderedit
 
 run: 
@@ -16,7 +27,7 @@ clean:
 	rm ./shaderedit
 
 init:
-	rm raylib.h raymath.h
+	rm -f raylib.h raymath.h
 	wget https://raw.githubusercontent.com/raysan5/raylib/refs/heads/master/src/raylib.h
 	wget https://raw.githubusercontent.com/raysan5/raylib/refs/heads/master/src/raymath.h
 
--- main.c
+++ main.c
@@ -1,17 +1,35 @@
 #include "raylib.h"
+#include "raymath.h"
+#include <math.h>
+#include <stdio.h>
 
 #define WIDTH 500
 #define HEIGHT 500
 
 // PREVIEW
-// 1. Setup cube to rotate
-typedef struct {
-  Vector3 position;
-  float size;
-  Color color;
-} Cube;
-// 2. rotate function
-// 3. Create 3D Viewport to render cube
+// 1. Setup cube to rotate              : DONE
+// 2. rotate function                   : DONE
+// 3. Create 3D Viewport to render cube : DONE
+// 4. Setup camera                      : DONE
+// 5. Render cube
+
+Vector3 cubePos = {0.0f, 0.0f, 0.0f};
+
+Camera camera = {
+    .position = {10.0f, 10.0f, 10.0f},
+    .target = {0.0f, 0.0f, 0.0f},
+    .up = {0.0f, 1.0f, 0.0f},
+    .fovy = 45.0f,
+    .projection = CAMERA_PERSPECTIVE,
+};
+
+void UpdateDrawCube3D() {
+  UpdateCamera(&camera, CAMERA_ORBITAL);
+  BeginMode3D(camera);
+  DrawCube(cubePos, 2.0f, 2.0f, 2.0f, RED);
+  DrawCubeWires(cubePos, 2.0f, 2.0f, 2.0f, WHITE);
+  EndMode3D();
+}
 
 // SHADERS
 // 1. Setup shader constants
@@ -20,16 +38,29 @@ typedef struct {
 // 4. File watchers setup for shader files
 // 5. Setup hot reloading
 
+// UTILS
+
+void DrawDebugText() {
+  DrawText("Shader text here...", 10, 10, 10, Fade(WHITE, 0.5f));
+  DrawFPS(10, WIDTH - 30);
+}
+
 int main(void) {
   InitWindow(WIDTH, HEIGHT, "[shadereditor]");
+  SetWindowState(FLAG_MSAA_4X_HINT);
+  SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(BLACK);
-    DrawText("Shader text here...", 10, 10, 20, RAYWHITE);
+
+    UpdateDrawCube3D();
+    DrawDebugText();
+
     EndDrawing();
   }
 
   CloseWindow();
+
   return 0;
 }
